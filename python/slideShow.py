a02f22df5edbe2c7c790ede84a7f8c16a428c8c6
Sanjeev Kumar <skysan@gmail.com>
2013-11-26 19:11:03 +0530

    moved logic from slideshow to slideshowbase class
    modified call to main() function

--- python/slideShow.py
+++ python/slideShow.py
@@ -1,53 +1,50 @@
-"""
-This program is licensed under the BSD license.
+"""	This program is licensed under the BSD license.
 
-Copyright (c) 2013, Sanjeev Kumar
-All rights reserved.
+	Copyright (c) 2013, Sanjeev Kumar
+	All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
+	Redistribution and use in source and binary forms, with or without modification, 
+	are permitted provided that the following conditions are met:
 
-	* Redistributions of source code must retain the above copyright notice, 
-	  this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above copyright notice, 
-	  this list of conditions and the following disclaimer in the documentation 
-	  and/or other materials provided with the distribution.
-	* Neither the name of the Dino Interactive nor the names of its contributors 
-	  may be used to endorse or promote products derived from this software 
-	  without specific prior written permission.
+		* Redistributions of source code must retain the above copyright notice, 
+		  this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright notice, 
+		  this list of conditions and the following disclaimer in the documentation 
+		  and/or other materials provided with the distribution.
+		* Neither the name of the Dino Interactive nor the names of its contributors 
+		  may be used to endorse or promote products derived from this software 
+		  without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import sys
 import os
 import utils
 from PyQt4 import QtGui,QtCore
+import slideShowBase
 
 
-class SlideShowPics(QtGui.QMainWindow):
+class SlideShowPics(QtGui.QMainWindow, slideShowBase.SlideShowBase):
 
 	"""	SlideShowPics class defines the methods for UI and
 		working logic
 	"""
-	def __init__(self, imgLst, num=0, flag=True, ui=True, parent=None):
+	def __init__(self, imgLst, num=0, flag=True, parent=None):
 		super(SlideShowPics, self).__init__(parent)
-		self._imagesInList = imgLst
-		self._pause = False
-		self._count = num
-		self.animFlag = flag
-		self.prepairWindow(ui)
+		slideShowBase.SlideShowBase.__init__(self, imgLst=imgLst, ppState=False, count=num, animFlag=flag)
+		self.prepairWindow()
 
-	def prepairWindow(self, ui):
+	def prepairWindow(self):
 		if not self._imagesInList:
 			msgBox = QtGui.QMessageBox()
 			msgBox.setText("No Image found." )
@@ -56,22 +53,19 @@ class SlideShowPics(QtGui.QMainWindow):
 				self.populateImagestoSlideShow(self._browseDir())
 			else:
 				sys.exit()
-
-		if ui:
-			# Centre UI
-			screen = QtGui.QDesktopWidget().screenGeometry(self)
-			size = self.geometry()
-			self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
-			self.setStyleSheet("QWidget{background-color: #000000;}")
-			self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-			self._buildUi()
-			self.updateTimer = QtCore.QTimer()
-			self.connect(self.updateTimer, QtCore.SIGNAL("timeout()"), self.nextImage)
-			self.showFullScreen()
-			self.playPause()
-			#Shows the first image
-			self.showImageByPath(self._imagesInList[0])
-		# self.nextImage()
+		# Centre UI
+		screen = QtGui.QDesktopWidget().screenGeometry(self)
+		size = self.geometry()
+		self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
+		self.setStyleSheet("QWidget{background-color: #000000;}")
+		self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+		self._buildUi()
+		self.updateTimer = QtCore.QTimer()
+		self.connect(self.updateTimer, QtCore.SIGNAL("timeout()"), self.nextImage)
+		self.showFullScreen()
+		self.playPause()
+		#Shows the first image
+		self.showImageByPath(self._imagesInList[0])
 
 	def _buildUi(self):
 		self.label = QtGui.QLabel()
@@ -87,24 +81,9 @@ class SlideShowPics(QtGui.QMainWindow):
 		else:
 			sys.exit()
 
-	def populateImagestoSlideShow(self, path):
-		self._imagesInList = utils.imageFilePaths([path])
-
-
 	def nextImage(self):
-		"""	switch to next image or previous image
-		"""
-		if self._imagesInList:
-			if self._count == len(self._imagesInList):
-				self._count = 0
-
+		super(SlideShowPics, self).nextImage()
 		self.showImageByPath(self._imagesInList[self._count])
-
-		if self.animFlag:
-			self._count += 1
-		else:
-			self._count -= 1
-
 
 	def showImageByPath(self, path):
 		if path:
@@ -114,15 +93,6 @@ class SlideShowPics(QtGui.QMainWindow):
 					self.label.size(),
 					QtCore.Qt.KeepAspectRatio,
 					QtCore.Qt.SmoothTransformation))
-
-	def playPause(self):
-		if not self._pause:
-			self._pause = True
-			self.updateTimer.start(2500)
-			return self._pause
-		else:
-			self._pause = False
-			self.updateTimer.stop()
 
 	def keyPressEvent(self, keyevent):
 		"""	Capture key to exit, next image, previous image,
@@ -140,16 +110,7 @@ class SlideShowPics(QtGui.QMainWindow):
 		if event == 32:
 			self._pause = self.playPause()
 
-def main(paths):
-	if isinstance(paths, list):
-		imgLst = utils.imageFilePaths(paths)
-	elif isinstance(paths, str):
-		imgLst =  utils.imageFilePaths([paths])
-	else:
-		print " You can either enter a list of paths or single path"
-	beginShow(imgLst)
-
-def beginShow(imgLst=None):
+def main(imgLst=None):
 	app = QtGui.QApplication(sys.argv)
 	window =  SlideShowPics(imgLst)
 	window.show()
@@ -160,4 +121,4 @@ if __name__ == '__main__':
 	curntPaths = os.getcwd()
 	if len(sys.argv) > 1:
 		curntPaths = sys.argv[1:]
-	main(curntPaths)
+	main(slideShowBase.ingestData(curntPaths))
